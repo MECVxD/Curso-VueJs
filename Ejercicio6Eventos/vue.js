6a8333e0ecce2050b840beeb48435dcd914c2110
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-29 21:56:23 -0600

    Carpeta Ejercicios Eventos

---
+++ Ejercicio6Eventos/vue.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#eventos',
+    data:{
+        contador: 0,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        sumarUno: function(){
+            this.contador+=1
+        },
+        restarUno:function(){
+            this.contador-=1
+        },
+        alerta: function(mensaje){
+            alert(mensaje)
+        },
+        mostrarUbicacion: function(evento){
+            this.x=evento.clientX
+            this.y=evento.clientY
+        },
+        sumar: function(){
+            this.contador++
+        }
+    }
+})

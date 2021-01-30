da525cc4e0511522b55fd81645d63ba2d17d232d
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-29 21:54:08 -0600

    Carpeta Ejercicio 2

---
+++ Ejercicio2/vue.js
@@ -0,0 +1,35 @@
+new Vue({
+    el: "#vm",
+    data:{
+        mensaje:"Este es el mensaje"
+    },
+    beforeCreate: function(){
+        console.log('Llamando beforeCreate')
+    },
+    created: function(){//se utiliza mas a menudo
+        console.log('Llamando created')
+    },
+    beforeMount: function(){
+        console.log('Llamando beforeMount')
+    },
+    mounted: function(){
+        console.log('Llamando mounted')
+    },
+    beforeUpdate: function(){
+        console.log('Llamando beforeUpdate')
+    },
+    updated: function(){
+        console.log('Llamando updated')
+    },
+    beforeDestroy: function(){
+        console.log('Llamando beforeDestroy')
+    },
+    destroyed: function(){
+        console.log('Llamando destroyed')
+    },
+    methods:{
+        destruir: function(){
+            this.$destroy();//"$" algo que no creamos
+        }
+    }
+})
